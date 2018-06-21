e961adc4f2bee11087b98b994b319e39fc7b5c9f
Boyko Katranchev <32291114+boykopk@users.noreply.github.com>
2018-06-21 10:38:07 +0300

    Lab 3

---
+++ JS Advanced/Lab3 jQuery Library/select-towns.js
@@ -0,0 +1,23 @@
+function attachEvents() {
+    $('#items').on('click','li', function () {
+        let li = $(this);
+        if (li.attr('data-selected')){
+            li.removeAttr('data-selected');
+            li.css('background', '');
+        } else {
+            li.attr('data-selected', 'true');
+            li.css('background', '#DDD');
+        }
+    });
+
+    $('#showTownsButton').on('click', showTowns);
+
+    function showTowns() {
+        let selectedLi = $('#items li[data-selected=true]');
+        let towns = selectedLi.toArray()
+            .map(li => li.textContent)
+            .join(', ');
+
+        $('#selectedTowns').text('Selected towns: ' + towns);
+    }
+}
